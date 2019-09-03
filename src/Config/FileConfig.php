a12d7c68c98bf4037c1eae67a6f4d11d77c2672b
evalor <mipone@foxmail.com>
2019-09-03 17:00:46 +0800

    驱动类抽象

---
+++ src/Config/FileConfig.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Config;
+
+use EasySwoole\Spl\SplBean;
+
+/**
+ * 文件缓存配置
+ * Class FileConfig
+ * @package Config
+ */
+class FileConfig extends SplBean
+{
+    protected $cachePath;
+    protected $cachePrefix;
+    protected $defaultExpire;
+
+    function __construct(array $data = null, $autoCreateProperty = false)
+    {
+        $this->initDefaultConfig();
+        parent::__construct($data, $autoCreateProperty);
+    }
+
+    /**
+     * 设置默认的配置值
+     * @return void
+     */
+    private function initDefaultConfig()
+    {
+        $this->cachePath = sys_get_temp_dir();
+        $this->cachePrefix = 'SW_CACHE_';
+        $this->defaultExpire = 0;
+    }
+
+    /**
+     * 获取缓存目录
+     * @return mixed
+     */
+    public function getCachePath()
+    {
+        return $this->cachePath;
+    }
+
+    /**
+     * 设置缓存目录
+     * @param string $cachePath
+     */
+    public function setCachePath(string $cachePath): void
+    {
+        $this->cachePath = $cachePath;
+    }
+
+    /**
+     * 获取缓存前缀
+     * @return mixed
+     */
+    public function getCachePrefix()
+    {
+        return $this->cachePrefix;
+    }
+
+    /**
+     * 设置缓存前缀
+     * @param string $cachePrefix
+     */
+    public function setCachePrefix(string $cachePrefix): void
+    {
+        $this->cachePrefix = $cachePrefix;
+    }
+
+    /**
+     * 获取缓存超时
+     * @return mixed
+     */
+    public function getDefaultExpire()
+    {
+        return $this->defaultExpire;
+    }
+
+    /**
+     * 设置缓存超时
+     * @param int $defaultExpire
+     */
+    public function setDefaultExpire(int $defaultExpire): void
+    {
+        $this->defaultExpire = $defaultExpire;
+    }
+}
--- src/Drivers/AbstractDriver.php
+++ src/Drivers/AbstractDriver.php
@@ -2,6 +2,7 @@
 
 namespace SwooleKit\Cache\Drivers;
 
+use Opis\Closure\SerializableClosure;
 use Psr\SimpleCache\CacheInterface;
 
 /**
@@ -12,4 +13,122 @@ use Psr\SimpleCache\CacheInterface;
 abstract class AbstractDriver implements CacheInterface
 {
 
+    /**
+     * 获取缓存
+     * @param string $key
+     * @param null $default
+     * @return mixed
+     */
+    abstract public function get($key, $default = null);
+
+    /**
+     * 设置缓存
+     * @param string $key
+     * @param mixed $value
+     * @param null $ttl
+     * @return bool
+     */
+    abstract public function set($key, $value, $ttl = null);
+
+    /**
+     * 删除缓存
+     * @param string $key
+     * @return bool
+     */
+    abstract public function delete($key);
+
+    /**
+     * 清空缓存
+     * @return bool
+     */
+    abstract public function clear();
+
+    /**
+     * 缓存是否存在
+     * @param string $key
+     * @return bool
+     */
+    abstract public function has($key);
+
+    /**
+     * 批量获取缓存
+     * @param iterable $keys
+     * @param null $default
+     * @return array|iterable
+     */
+    public function getMultiple($keys, $default = null)
+    {
+        $result = [];
+        foreach ($keys as $key) {
+            $result[$key] = $this->get($key, $default);
+        }
+        return $result;
+    }
+
+    /**
+     * 批量设置缓存
+     * @param iterable $values
+     * @param null $ttl
+     * @return bool
+     */
+    public function setMultiple($values, $ttl = null)
+    {
+        foreach ($values as $key => $val) {
+            $result = $this->set($key, $val, $ttl);
+            if (false === $result) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 批量删除缓存
+     * @param iterable $keys
+     * @return bool
+     */
+    public function deleteMultiple($keys)
+    {
+        foreach ($keys as $key) {
+            $result = $this->delete($key);
+            if (false === $result) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 序列化数据
+     * @param $data
+     * @return string
+     */
+    protected function serialize($data): string
+    {
+        $serialize = $this->options['serialize'][0] ?? function ($data) {
+                SerializableClosure::enterContext();
+                SerializableClosure::wrapClosures($data);
+                $data = \serialize($data);
+                SerializableClosure::exitContext();
+                return $data;
+            };
+        return $serialize($data);
+    }
+
+    /**
+     * 反序列化数据
+     * @param string $data
+     * @return mixed
+     */
+    protected function unserialize(string $data)
+    {
+        $unserialize = $this->options['serialize'][1] ?? function ($data) {
+                SerializableClosure::enterContext();
+                $data = \unserialize($data);
+                SerializableClosure::unwrapClosures($data);
+                SerializableClosure::exitContext();
+                return $data;
+            };
+        return $unserialize($data);
+    }
 }
